5e388f2dbd69efbad330adff131f1ae66ee08b81
NikitaHebbalkar <hebbalkarnikita@gmail.com>
2024-11-29 00:32:16 +0530

    Deploy Angular app to GitHub Pages

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: 'C:/Program Files/Git/employee-management-angular/',
   entryPoints: new Map([['', () => import('./main.server.mjs')]]),
 };
   
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -4,8 +4,8 @@ export default {
   inlineCriticalCss: true,
   routes: undefined,
   assets: new Map([
-['index.csr.html', {size: 459, hash: '297b40a854cac4255ff57163ac6c8e3f423d89891979200a3dad8541697f2c19', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 972, hash: 'be134bfd8b9da4346897f8c612c160b325d641ba96f675b40d91b85c0342769b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
+['index.csr.html', {size: 507, hash: '7ea23c47f9c84a1f5235baf0c97f63dbe67b646641cbe5635b66a0904072c1f0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 1020, hash: '0dbf59b507e9d3b6dd14af5210502968dea52d02e99dda7c9f25b70a99b3ea77', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
 ['styles-5INURTSO.css', {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}]
 ]),
   locale: undefined,

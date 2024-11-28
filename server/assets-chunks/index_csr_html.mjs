834ff11ba8caee9d0120ddb217ab12a7272c0f97
NikitaHebbalkar <hebbalkarnikita@gmail.com>
2024-11-28 23:59:07 +0530

    Deploy Angular app to GitHub Pages

---
+++ server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,13 @@
+export default `<!DOCTYPE html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <base href="/"> 
+  <title>Employee Management</title>
+<link rel="stylesheet" href="styles-5INURTSO.css"></head>
+<body ngcm="">
+  <app-root></app-root>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-R2YJEDZK.js" type="module"></script></body>
+</html>
+`;

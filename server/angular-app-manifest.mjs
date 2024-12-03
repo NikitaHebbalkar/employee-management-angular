42e39969ca320b257d9c253365063e3cdd29a5a8
NikitaHebbalkar <hebbalkarnikita@gmail.com>
2024-12-03 18:39:20 +0530

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -4,8 +4,8 @@ export default {
   inlineCriticalCss: true,
   routes: undefined,
   assets: new Map([
-['index.csr.html', {size: 520, hash: 'a23640a0160e16c0c2b227e6f5ba339fd36c6a77ff6457a652abc0c8494ba9f1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 1033, hash: 'cf97806fb33efdc1f2cfde73e19379b34c852c95f5c83773e96f1382b55f4b20', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
+['index.csr.html', {size: 519, hash: 'c44f7830d0e95ac2b5fb716299fcb69f5681d303a741b012131a5b705d2381cd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 1032, hash: '6c5d4fb3afea2578c45d9eabeca8f64e3ac469cde6035bd231aa6c7e979893ab', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
 ['styles-5INURTSO.css', {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}]
 ]),
   locale: undefined,
